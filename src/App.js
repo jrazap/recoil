dfa22f3bb4ed7e30e48039d1fb9246bb5255eb2e
jrazap <elazapm@gmail.com>
2024-05-31 10:43:20 +0200

    Change favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Helmet } from "react-helmet";
 import Todos from "./components/todos";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Recoil Tutorial | jrazap</title>
+      </Helmet>
+
       <div className="container">
         <h1 className="my-3 display-2">Learn Recoil</h1>
         <Todos />
